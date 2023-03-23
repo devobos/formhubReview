d82dea3daa0154a166e110676c50a93029daed5e
Philip <contact@philipbos.com>
2023-03-23 01:55:41 +0000

    fix href on dashboard button

--- firebase.js
+++ firebase.js
@@ -13,7 +13,9 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-const db = getFirestore(app);
+const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true,
+});
 const auth = getAuth(app);
 
 export { app, db, auth };
