c41d7cdca4ce416484b7548d32c3752a817b4d67
Philip <contact@philipbos.com>
2023-03-23 01:26:54 +0000

    fix href on dashboard button

--- app/page.js
+++ app/page.js
@@ -24,19 +24,31 @@ const Page = () => {
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
           <div className="">
-            <button
-              onClick={!user ? googleLogInHandler : undefined}
-              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-lime-500 hover:bg-red-700"
-            >
-              <span className="sr-only">
-                {user ? <>Your Dashboard</> : <>Sign in with Google</>}
-              </span>
+            {!user ? (
+              <button
+                onClick={googleLogInHandler}
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-lime-500 hover:bg-red-700"
+              >
+                <span className="sr-only">Sign in with Google</span>
 
-              <span className="ml-2">
-                {" "}
-                {user ? <>Your Dashboard</> : <>Sign in with Google</>}
-              </span>
-            </button>
+                <span className="ml-2">
+                  {" "}
+                  {user ? <>Your Dashboard</> : <>Sign in with Google</>}
+                </span>
+              </button>
+            ) : (
+              <a
+                href="/dashboard"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-lime-500 hover:bg-red-700"
+              >
+                <span className="sr-only">Sign in with Google</span>
+
+                <span className="ml-2">
+                  {" "}
+                  {user ? <>Your Dashboard</> : <>Sign in with Google</>}
+                </span>
+              </a>
+            )}
             {user && (
               <button
                 onClick={logout}
